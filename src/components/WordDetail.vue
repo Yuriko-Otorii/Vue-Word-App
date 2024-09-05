8303b26dfe5e16800489d8dfab2c315307d14faf
Yuri <104223310+Yuriko-Otorii@users.noreply.github.com>
2024-09-05 11:35:53 -0700

    Add word list display logic

--- src/components/WordDetail.vue
+++ src/components/WordDetail.vue
@@ -1,22 +1,21 @@
 <template>
-    <div class="flex flex-col items-start bg-white rounded-lg w-full h-fit m-3 py-8 px-5">
+    <div v-if="clickedWord === word.id" class="flex flex-col items-start bg-white rounded-lg w-full h-fit m-3 py-8 px-5 mx-auto">
       <div class="flex items-center justify-between bg-white rounded-lg w-full mx-auto mb-4">
-        <p class="text-3xl font-bold">deserted</p>
-        <ion-icon aria-hidden="true" :icon="starOutline" class="text-3xl text-gray-400" />
+        <p class="text-3xl font-bold">{{ word.word }}</p>
+        <ion-icon aria-hidden="true" :icon="star" class="text-3xl text-gray-300" />
       </div>
 
-      <p class="text-lg mb-1">adjective</p>
-      <p class="text-lg">Memory process:<span class="ml-1">1</span>/4</p>
+      <p class="text-lg mb-1">{{ word.category  }}</p>
+      <p class="text-lg">Memory process:<span class="ml-1">{{ word.memoryProcess }}</span>/4</p>
       
       <div class="w-full h-[2px] bg-[#4289A7] opacity-60 rounded-lg self-center my-5"></div>
       
       <label class="text-base text-gray-400">Definition:</label>
-      <p class="text-lg mb-8">If a place is deserted, there are no people in it.</p>
+      <p class="text-lg mb-8">{{ word.definition }}</p>
       
       <label class="text-base text-gray-400">Examples:</label>
-      <ul class="list-disc ml-4">
-        <li class="text-lg">a deserted building/street</li>
-        <li class="text-lg">The coastal resorts are deserted in winter.</li>
+      <ul class="">
+        <li class="text-lg">{{ word.example }}</li>
       </ul>
 
     </div>
@@ -24,6 +23,10 @@
 
 <script setup lang="ts">
   import { IonIcon } from '@ionic/vue';
-  import { starOutline } from 'ionicons/icons';
+  import { star } from 'ionicons/icons';
+  import { defineProps } from 'vue';
 
+  const { word, clickedWord } = defineProps(["word", "clickedWord"]);
+
+  
 </script>
--- src/views/WordListPage.vue
+++ src/views/WordListPage.vue
@@ -7,36 +7,44 @@
     </ion-header>
     <ion-content class="ion-padding">
       <div class="flex flex-col items-center">
-        <div class="flex items-center w-full mx-auto px-3">
+        <div class="flex items-center w-full mx-auto px-3 h-[30px]">
           <ion-icon v-if="phase !== 'selectCategory'" @click="handleBackArrow" aria-hidden="true" :icon="arrowBack" class="text-3xl text-gray-500" />
-          <ion-searchbar show-clear-button="focus" placeholder="Search word" class="custom-searchbar"></ion-searchbar>
-          <ion-icon v-if="phase === 'wordList'" aria-hidden="true" :icon="filter" class="text-3xl text-gray-500" />
+          <!-- <ion-searchbar show-clear-button="focus" placeholder="Search word" class="custom-searchbar"></ion-searchbar>
+          <ion-icon v-if="phase === 'wordList'" aria-hidden="true" :icon="filter" class="text-3xl text-gray-500" /> -->
         </div>
-        <h1 class="text-xl text-center">Category:<span v-if="phase !== 'selectCategory'" class="ml-1">Animal</span></h1>
+        <h1 class="text-xl text-center">Category:<span v-if="phase !== 'selectCategory'" class="text-xl ml-1 font-bold">{{ selectedCategoryName }}</span></h1>
         <ion-list v-if="phase === 'selectCategory'" class="w-full flex flex-col gap-2">
           <div
             v-for="category in categoryList"
             :key="category.id"
-            @click="handleSlectCategory"
+            @click="handleSlectCategory(category.name)"
             class="flex items-center justify-between bg-white rounded-lg w-full h-[80px] py-3 px-6 mx-auto"
           >
             <p>{{ category.name }}</p>
-            <p><span>10</span>items</p>              
+            <p>
+              <span v-if="category.name === 'adjective'">{{ wordsInAsjectives.length }}</span>
+              <span v-if="category.name === 'animal'">{{ wordsInAnimals.length }}</span>
+              <span v-if="category.name === 'phrasal verb'">{{ wordsInPhrasalVerbs.length }}</span>
+              <span v-if="category.name === 'doctors'">{{ wordsInDoctors.length }}</span>
+              <span v-if="category.name === 'Business'">0</span>
+              items
+            </p>              
           </div>
         </ion-list>
         
-        <ion-list v-if="phase === 'wordList'" class="w-full flex flex-col gap-4">
-          <div
-            v-for="word in words"
-            :key="word"
-            @click="handleWordDetail"
-            class="flex items-center justify-between bg-white rounded-lg w-full h-[50px] py-3 px-6 mx-auto"
-          >
-            <p>{{ word }}</p>
-          </div>
-        </ion-list>
+        <div v-for="word in selectedCategory" :key="word.id" class="w-full">
+          <ion-list v-if="phase === 'wordList'" class="w-full flex flex-col gap-4">
+            <div              
+              @click="handleWordDetail(word.id)"
+              class="flex items-center justify-between bg-white rounded-lg w-full h-[50px] py-3 px-6 mx-auto"
+            >
+              <p>{{ word.word }}</p>
+            </div>
+          </ion-list>
+          <WordDetail v-if="phase === 'wordDetail'" :word="word" :clickedWord="clickedWord"/>
+        </div>
 
-        <WordDetail v-if="phase === 'wordDetail'" />
+        
 
       </div>
   
@@ -45,8 +53,8 @@
 </template>
 
 <script setup lang="ts">
-    import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonIcon, IonList } from '@ionic/vue';
-    import { arrowBack, filter } from 'ionicons/icons';
+    import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList } from '@ionic/vue';
+    import { arrowBack } from 'ionicons/icons';
     import { ref, onMounted } from 'vue';
     import axios from 'axios';
 
@@ -60,8 +68,13 @@
 
     const categoryList = ref<category[]>([]);
     const words = ref([]);
-    const wordsInAnimals = ref([]);
-    // const words = ["Dog", "Cat", "Raabbit", "Fox", "Pocupine", "Elephant", "Giraffe", "Lion", "Tiger", "Bear", "Wolf", "Deer", "Horse", "Cow", "Pig", "Sheep", "Goat", "Chicken", "Duck", "Goose", "Turkey", "Pigeon", "Parrot", "Ostrich", "Eagle", "Hawk", "Falcon", "Owl", "Swan", "Pelican", "Stork", "Crane", "Flamingo", "Penguin", "Seagull", "Albatross", "Sparrow", "Starling", "Crow", "Raven", "Magpie", "Jay", "Chickade"];
+    const wordsInAsjectives = ref<CardItem[]>([]);
+    const wordsInAnimals = ref<CardItem[]>([]);
+    const wordsInPhrasalVerbs = ref<CardItem[]>([]);
+    const wordsInDoctors = ref<CardItem[]>([]);
+    const selectedCategory = ref<CardItem[]>([]);
+    const selectedCategoryName = ref<string>("");
+    const clickedWord = ref<number>();
     const phase = ref("selectCategory");
 
     // const wordsInAnimals = words.value.filter((eachItem: CardItem[]) => {
@@ -73,12 +86,25 @@
     // console.log(wordsInAnimals);
     
 
-    const handleSlectCategory = () => {
+    const handleSlectCategory = (category: string) => {
+      selectedCategoryName.value = category;
+      if (category === "animal") {
+        selectedCategory.value = wordsInAnimals.value;
+      } else if (category === "adjective") {
+        selectedCategory.value = wordsInAsjectives.value;
+      } else if (category === "phrasal verb") {
+        selectedCategory.value = wordsInPhrasalVerbs.value;
+      } else if (category === "doctors") {
+        selectedCategory.value = wordsInDoctors.value;
+      } else {
+        selectedCategory.value = [];
+      }
       phase.value = "wordList"
     }
 
-    const handleWordDetail = () => {
+    const handleWordDetail = (wordId: number) => {
       phase.value = "wordDetail"
+      clickedWord.value = wordId;
     }
 
     const handleBackArrow = () => {
@@ -98,21 +124,15 @@
 
         categoryList.value = response1.data;
         words.value = response2.data;
+        console.log(response2.data);
 
-        wordsInAnimals.value = words.value.filter((eachItem: CardItem[]) => {
-          console.log(eachItem);
-          
-          eachItem.filter((item: CardItem) => {
-            return item.category === "Animal";
-          });
-        });
+        const [words1, words2, words3, words4] = words.value;
 
-        console.log(wordsInAnimals.value);
+        wordsInAsjectives.value = words1;
+        wordsInAnimals.value = words2;
+        wordsInPhrasalVerbs.value = words3;
+        wordsInDoctors.value = words4;
         
-
-        // const response = await axios.get('http://localhost:5000/category');
-        // categoryList.value = response.data;
-        // console.log(response.data);        
       } catch (error) {
         console.log(error);
       }
